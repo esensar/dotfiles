f4b54754339f5cbe30f55ec827855c528472a535
Ensar Sarajčić <es.ensar@gmail.com>
2021-01-19 08:28:27 +0100

    Add termux support in fish clipboard

--- symlinks/config/fish/platform_config/linux.fish
+++ symlinks/config/fish/platform_config/linux.fish
@@ -1,2 +1,7 @@
-alias pbcopy 'wl-copy'
-alias pbpaste 'wl-paste'
+if which termux-info  &> /dev/null
+	alias pbcopy 'termux-clipboard-set'
+	alias pbpaste 'termux-clipboard-get'
+else
+	alias pbcopy 'wl-copy'
+	alias pbpaste 'wl-paste'
+end

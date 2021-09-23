796c7b3ee7d10fb90893a2322d85495847e3f779
Ensar Sarajčić <es.ensar@gmail.com>
2021-09-23 13:06:48 +0200

    Use cucumber-language-server from lspconfig

--- symlinks/config/nvim/lua/lsp/servers.lua
+++ symlinks/config/nvim/lua/lsp/servers.lua
@@ -3,7 +3,6 @@
 -------------------------------------------------------------------------------
 
 local lspconfig = require("lspconfig")
-require("lsp.extra_servers.cucumber")
 
 local on_attach = function(client, bufnr)
 	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -27,7 +26,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Lsp default language servers
-local servers = { "bashls", "clangd", "cucumberls", "dockerls", "jsonls", "pyright", "rust_analyzer", "kotlin_language_server", "vimls", "clojure_lsp", "gopls", "gdscript", "terraformls", "tsserver" }
+local servers = { "bashls", "clangd", "cucumber_language_server", "dockerls", "jsonls", "pyright", "rust_analyzer", "kotlin_language_server", "vimls", "clojure_lsp", "gopls", "gdscript", "terraformls", "tsserver" }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup { on_attach = on_attach }
 end

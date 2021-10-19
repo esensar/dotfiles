513b436019cc3777faa255dcd2bac90994158c8f
Ensar Sarajčić <es.ensar@gmail.com>
2021-10-19 11:16:33 +0200

    Add support for linux pbcopy on X11 too

--- symlinks/config/fish/platform_config/linux.fish
+++ symlinks/config/fish/platform_config/linux.fish
@@ -1,7 +1,10 @@
 if which termux-info  &> /dev/null
 	alias pbcopy 'termux-clipboard-set'
 	alias pbpaste 'termux-clipboard-get'
-else
+else if set -q WAYLAND_DISPLAY; and type -q wl-paste; and type -q wl-copy
 	alias pbcopy 'wl-copy'
 	alias pbpaste 'wl-paste'
+else
+	alias pbcopy 'xsel -i'
+	alias pbpaste 'xsel -o'
 end

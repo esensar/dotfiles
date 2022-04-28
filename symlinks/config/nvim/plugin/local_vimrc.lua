93928177f178066ac5988d8e1579e5a2a46ffb76
Ensar Sarajčić <es.ensar@gmail.com>
2022-04-28 08:56:17 +0200

    Add helpers for local vimrc

---
+++ symlinks/config/nvim/plugin/local_vimrc.lua
@@ -0,0 +1,5 @@
+-------------------------------------------------------------------------------
+--    - Local vimrc commands -
+-------------------------------------------------------------------------------
+
+vim.api.nvim_create_user_command("EditLocalVimrc", function(_) require"direnv_vimrc_local".open_local_vimrc() end, { nargs = 0 })

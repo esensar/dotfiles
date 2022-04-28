e3301f6240d6ed56933f9cd63c1f8f73cc9a6c46
Ensar Sarajčić <es.ensar@gmail.com>
2022-04-28 17:13:58 +0200

    Add force parameter to automatic packer download

--- symlinks/config/nvim/lua/init/first_load.lua
+++ symlinks/config/nvim/lua/init/first_load.lua
@@ -1,7 +1,9 @@
 -- Adopted from https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/lua/tj/first_load.lua
-local download_packer = function()
-	if vim.fn.input("Download Packer? (y for yes)") ~= "y" then
-		return
+local download_packer = function(force)
+	if not force then
+		if vim.fn.input("Download Packer? (y for yes)") ~= "y" then
+			return
+		end
 	end
 
 	local directory = string.format("%s/site/pack/packer/start/", vim.fn.stdpath("data"))
@@ -17,11 +19,14 @@ local download_packer = function()
 	vim.api.nvim_command("PackerCompile")
 	vim.api.nvim_command("PackerInstall")
 	print("( You'll need to restart now )")
+	if force then
+		vim.api.nvim_command("quitall")
+	end
 end
 
-return function()
+return function(force)
 	if not pcall(require, "packer") then
-		download_packer()
+		download_packer(force)
 
 		return true
 	end

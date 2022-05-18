fb18155176d0d0b980d72edcc7bbb76e4f3a4762
Ensar Sarajčić <es.ensar@gmail.com>
2022-05-18 16:02:31 +0200

    Fix signature_help mapping in ideavim and neovim

--- symlinks/config/nvim/lua/esensar/lsp/server_config.lua
+++ symlinks/config/nvim/lua/esensar/lsp/server_config.lua
@@ -6,7 +6,6 @@ local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protoco
 
 local function on_attach(client, bufnr)
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
 	-- Lsp keymaps
 	local opts = { buffer = bufnr }
 	vim.keymap.set("n", "<C-]>", vim.lsp.buf.definition, opts)
@@ -14,7 +13,7 @@ local function on_attach(client, bufnr)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 	vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
-	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+	vim.keymap.set({ "n", "i" }, "<A-k>", vim.lsp.buf.signature_help, opts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 	vim.keymap.set("n", "<A-CR>", vim.lsp.buf.code_action, opts)
 	vim.keymap.set("n", "<Leader>ac", vim.lsp.buf.code_action, opts)

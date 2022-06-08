84d61aa1d21df368385c9e4aac4229ddb84f2a6a
Ensar Sarajčić <es.ensar@gmail.com>
2022-06-08 09:50:23 +0200

    Enable backup and swapfile

--- symlinks/config/nvim/lua/esensar/init/options.lua
+++ symlinks/config/nvim/lua/esensar/init/options.lua
@@ -1,10 +1,10 @@
 local opt = vim.opt
 
+-- Not needed?
 opt.wildignore = "__pycache__"
 opt.wildignore = opt.wildignore + { "*.o", "*~", "*.pyc", "*pycache*" }
 
 -- Indentation config
-opt.tabstop = 2
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.autoindent = true
@@ -16,11 +16,9 @@ opt.relativenumber = true
 -- Misc
 opt.backspace = "indent,eol,start" -- Allow backspacing over everything in insert mode
 opt.ttimeoutlen = 50
-opt.backup = false -- do not keep a backup file, use versions instead
 opt.history = 50 -- Keep 50 lines of command line history
 opt.ruler = true -- Show the cursor position all the time
 opt.laststatus = 2 -- Always display the status line
-opt.swapfile = false -- Disable swapfile
 opt.undofile = true -- Enable undofile (auto supported in NeoVim)
 opt.showcmd = true -- Display incomplete commands
 opt.incsearch = true -- Do incremental searching

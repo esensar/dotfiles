3948c77b1fd75e61bbf492662c99623897dfa037
Ensar Sarajčić <dev@ensarsarajcic.com>
2022-07-21 19:08:50 +0200

    Fix cloning scripts in Makefile

--- symlinks/config/fish/config.fish
+++ symlinks/config/fish/config.fish
@@ -19,6 +19,7 @@ alias cat "bat"
 if which termux-info &> /dev/null
 else
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  export MANROFFOPT="-c"
 end
 
 abbr -a batdiff "git diff --name-only --diff-filter=d | xargs bat --diff"

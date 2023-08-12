e1157e455fe25213188988089608734f7fb4006c
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-08-12 20:54:10 +0200

    Improve autocompletion for t command

--- symlinks/config/fish/completions/t.fish
+++ symlinks/config/fish/completions/t.fish
@@ -2,6 +2,10 @@ function __fish-timesheets-available-projects
     grep 'account Project:.*' "$TIMESHEET_LEDGER_HOME/config/02_accounts.dat" | cut -f2 -d ' '
 end
 
+function __fish-timesheets-running-projects
+    cat "$TIMESHEET_LEDGER_HOME/timesheets/$(date +%Y).journal" | grep -v ';' | awk '{ last[$4] = $1 } END { for (i in last) { if (last[i] == "i") { print i } } }'
+end
+
 function __fish-timesheets-existing-comments
     ledger -f "$TIMESHEET_LEDGER_HOME/main.journal" payees ^Project
 end
@@ -22,7 +26,7 @@ complete -c t -x -n "__fish_seen_subcommand_from in" -n "__fish_timesheets_arg_n
 complete -c t -x -n "__fish_seen_subcommand_from in" -n "__fish_timesheets_arg_number 3" -a '(__fish-timesheets-existing-comments)'
 
 # Out
-complete -c t -x -n "__fish_seen_subcommand_from out" -a '(__fish-timesheets-available-projects)'
+complete -c t -x -n "__fish_seen_subcommand_from out" -a '(__fish-timesheets-running-projects)'
 
 # Bal
 complete -c t -x -n "__fish_seen_subcommand_from bal" -a '(__fish-timesheets-available-projects)'

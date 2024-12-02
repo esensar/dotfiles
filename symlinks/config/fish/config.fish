1e5a9679f6c5d725deff24f820f9d917d7491322
Ensar Sarajčić <dev@ensarsarajcic.com>
2024-12-02 16:34:02 +0100

    Fix fish agnoster theme prompt width

--- symlinks/config/fish/config.fish
+++ symlinks/config/fish/config.fish
@@ -1,6 +1,7 @@
 fenv source ~/.profile
 
 set -g fish_prompt_pwd_dir_length 0
+set -g fish_vcs_branch_name_length 0
 set -gx PROJECT_PATHS ~/Projects/*/* ~/Projects/Personal/Mixed\ Technology/Practice
 set -gx MISE_PYTHON_DEFAULT_PACKAGES_FILE ~/.config/asdf/.python-default-packages
 set -gx FZF_DEFAULT_COMMAND 'rg --files'

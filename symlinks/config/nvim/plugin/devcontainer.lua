47e2389d08293cc485cffed1da3c5b0011618a2f
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-10-15 19:35:04 +0200

    Switch to rofi from ulauncher

--- symlinks/config/nvim/plugin/devcontainer.lua
+++ symlinks/config/nvim/plugin/devcontainer.lua
@@ -5,4 +5,25 @@ require("devcontainer").setup({
 		init = true,
 		update = true,
 	},
+	attach_mounts = {
+		neovim_config = {
+			-- enables mounting local config to /root/.config/nvim in container
+			enabled = true,
+			-- makes mount readonly in container
+			options = { "readonly" },
+		},
+		neovim_data = {
+			-- enables mounting local data to /root/.local/share/nvim in container
+			enabled = true,
+			-- no options by default
+			options = {},
+		},
+		-- Only useful if using neovim 0.8.0+
+		neovim_state = {
+			-- enables mounting local state to /root/.local/state/nvim in container
+			enabled = true,
+			-- no options by default
+			options = {},
+		},
+	},
 })
